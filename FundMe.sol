01751c8650aae3f483160bedbab9ea2686b02383
BowtiedHarpyEagle <bowtiedharpyeagle@proton.me>
2024-10-01 15:55:16 -0500

    complete lesson 3 payable

--- FundMe.sol
+++ FundMe.sol
@@ -3,7 +3,9 @@ pragma solidity ^0.8.18;
 
 contract FundMe{
     
-    function fund() public {}
+    function fund() public payable {
+        require(msg.value > 1e18, "didn't send enough");
+    }
 
     function withdraw() public {}
 }
