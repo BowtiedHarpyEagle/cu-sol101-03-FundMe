a4125543f21bbb2209441bbae9059e4133750a0f
BowtiedHarpyEagle <bowtiedharpyeagle@proton.me>
2024-10-02 10:37:24 -0500

    L29 deploy to Remix

--- contracts/PriceConverter.sol
+++ contracts/PriceConverter.sol
@@ -1,14 +1,15 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+pragma solidity 0.8.24;
 
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 library PriceConverter {
 
     function getPrice() internal view returns(uint256){
-        // Address 0x5fb1616F78dA7aFC9FF79e0371741a747D2a7F22
+        // Sepolia Address 0x5fb1616F78dA7aFC9FF79e0371741a747D2a7F22
+        // ZKsync sepolia 0xfEefF7c3fB57d18C5C6Cdd71e45D2D0b4F9377bF
         // ABI 
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x5fb1616F78dA7aFC9FF79e0371741a747D2a7F22);
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0xfEefF7c3fB57d18C5C6Cdd71e45D2D0b4F9377bF);
         (, int256 price, , , ) = priceFeed.latestRoundData();
         return uint256(price * 1e10);
 
@@ -21,6 +22,6 @@ library PriceConverter {
     }
 
     function getVersion() internal view returns(uint256) {
-        return AggregatorV3Interface(0x5fb1616F78dA7aFC9FF79e0371741a747D2a7F22).version();
+        return AggregatorV3Interface(0xfEefF7c3fB57d18C5C6Cdd71e45D2D0b4F9377bF).version();
     }
 }
